4aaad99f4d08e117333cc6f6c63758c7d4bbc8f5
Rafał Hirsz <rafal@hirsz.co>
2015-12-30 00:43:08 +0100

    Add trigonometric and unnamed variations

--- src/variations/mod.rs
+++ src/variations/mod.rs
@@ -7,6 +7,14 @@ pub fn make_variation(name: &str, params: &[Number]) -> Result<Box<Variation>, &
             if params.len() < 4 { Err("Not enough parameters") }
             else { Ok(Box::new(DeJong(params[0], params[1], params[2], params[3]))) }
         },
+        "Trigonometric" => {
+            if params.len() < 8 { Err("Not enough parameters") }
+            else { Ok(Box::new(Trigonometric(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7]))) }
+        },
+        "Unnamed" => {
+            if params.len() < 4 { Err("Not enough parameters") }
+            else { Ok(Box::new(Unnamed(params[0], params[1], params[2], params[3]))) }
+        }
         _ => Err("Invalid variation")
     }
 }
@@ -28,6 +36,30 @@ impl Variation for DeJong {
         Point::from_xy(
             (self.0 * point.y).sin() - (self.1 * point.x).cos(),
             (self.2 * point.x).sin() - (self.3 * point.y).cos()
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct Trigonometric(pub Number, pub Number, pub Number, pub Number, pub Number, pub Number, pub Number, pub Number);
+
+impl Variation for Trigonometric {
+    fn apply(&self, point: &Point, _: &AffineTransformation) -> Point {
+        Point::from_xy(
+            self.0 * (self.1 * point.y).sin() + self.2 * (self.3 * point.x).cos(),
+            self.4 * (self.5 * point.x).sin() + self.6 * (self.7 * point.y).cos()
+        )
+    }
+}
+
+#[derive(Debug)]
+pub struct Unnamed(pub Number, pub Number, pub Number, pub Number);
+
+impl Variation for Unnamed {
+    fn apply(&self, point: &Point, _: &AffineTransformation) -> Point {
+        Point::from_xy(
+            point.y + self.0 * point.x.signum() * (self.1 * point.x - self.2).abs().sqrt(),
+            self.3 - point.x
         )
     }
 }
